ac9013b2664df5e926b46b024640d3b8e9c530c1
NMunro <neilmunro@gmail.com>
2021-07-07 12:12:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { Sequelize } = require("sequelize");
+const app = express();
+
+require("dotenv").config();
+
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialiectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
+
+app.get("/", async(req, res) => {
+    res.status(200).json({"status": "ok"});
+});
+
+const server = app.listen(process.env.PORT || 5000, async() => {
+    db.authenticate();
+    console.log("App is online");
+});
+
+const shutdown = () => {
+    server.close(async() => {
+        console.log("App is now offline.");
+        await db.close();
+    });
+};
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM");
+    shutdown();
+});
+
+process.on("SIGINT", () => {
+    console.log("SIGINT");
+    shutdown();
+});
